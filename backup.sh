0e68030eddab3b6e3ae8ab09aecc6a11fbe1fae9
Jay <mohamya77@gmail.com>
2025-03-07 00:58:08 +0200

    changes in backup script

--- backup.sh
+++ backup.sh
@@ -1,57 +1,42 @@
 #!/bin/bash
 
-# This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
+# Check if the number of arguments is correct
 if [[ $# != 2 ]]
 then
   echo "backup.sh target_directory_name destination_directory_name"
   exit
 fi
 
-# This checks if argument 1 and argument 2 are valid directory paths
+# Check if argument 1 and argument 2 are valid directory paths
 if [[ ! -d $1 ]] || [[ ! -d $2 ]]
 then
   echo "Invalid directory path provided"
   exit
 fi
 
-# [TASK 1]
 targetDirectory=$1
 destinationDirectory=$2
-# [TASK 2]
+
 echo "argument1 is $1"
 echo "argument2 is $2"
 
-# [TASK 3]
-currentTS=$(date +%s)	
+currentTS=$(date +%s)
 
-# [TASK 4]
 backupFileName="backup-[$currentTS].tar.gz"
 
-# We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
-
-# To make things easier, we will define some useful variables...
-
-# [TASK 5]
 origAbsPath=$(pwd)
 
-# [TASK 6]
-cd "$destinationDirectory" 	 
+cd "$destinationDirectory" 
 destDirAbsPath=$(pwd)
 
-# [TASK 7]
 cd "$origAbsPath" 
 cd "$targetDirectory"
 
-# [TASK 8]
 yesterdayTS=$(($currentTS - 24 * 60 * 60))
 
 declare -a toBackup
 
-for file in $(ls)  #[TASK 9]
+for file in $(ls)
 do
 if [ $(date -r "$file" +%s) -gt "$yesterdayTS" ]
   then
@@ -59,10 +44,7 @@ if [ $(date -r "$file" +%s) -gt "$yesterdayTS" ]
   fi
 done
 
-# [TASK 12]
 tar -czvf "$backupFileName" "${toBackup[@]}"
-# [TASK 13]
-mv $backupFileName $destDirAbsPath
 
-# Congratulations! You completed the final project for this course!
+mv $backupFileName $destDirAbsPath
 
